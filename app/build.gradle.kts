ba581a29ddcc171aa532d4ebdf48382fb5c476e6
PixelyIon <pixelyion@protonmail.com>
2023-12-09 13:59:44 +0530

    Add Kotlinx JSON Serialization plugin
    We'll be using JSON as a database format throughout the codebase and need reliable parsing for it.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,6 +1,7 @@
 plugins {
     id("com.android.application")
     id("org.jetbrains.kotlin.android")
+    kotlin("plugin.serialization")
 }
 
 android {
@@ -76,6 +77,7 @@ dependencies {
     implementation("androidx.compose.ui:ui-tooling-preview")
     implementation("androidx.compose.material3:material3")
     implementation("androidx.core:core-splashscreen:1.0.1")
+    implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.6.0")
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
--- build.gradle.kts
+++ build.gradle.kts
@@ -3,4 +3,5 @@ plugins {
     id("com.android.application") version "8.3.0-alpha04" apply false
     id("org.jetbrains.kotlin.android") version "1.8.10" apply false
     kotlin("jvm") version "1.9.20" apply false
+    kotlin("plugin.serialization") version "1.9.20" apply false
 }
