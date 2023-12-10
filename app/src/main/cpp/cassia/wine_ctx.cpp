73e9691a2d690682704a07697d3721a458939209
PixelyIon <pixelyion@protonmail.com>
2023-12-10 17:07:29 +0530

    Update C++ code style
    * Namespaces no longer have any indentation
    * Visibility modifiers now have a 2 space indentation

--- app/src/main/cpp/cassia/wine_ctx.cpp
+++ app/src/main/cpp/cassia/wine_ctx.cpp
@@ -3,21 +3,21 @@
 #include "wine_ctx.h"
 
 namespace cassia {
-    WineContext::WineContext(std::filesystem::path pRuntimePath, std::filesystem::path pPrefixPath)
-            : runtimePath{std::move(pRuntimePath)}, prefixPath{std::move(pPrefixPath)},
-              envVars{"WINEPREFIX=" + (prefixPath / "pfx").string(), "HOME=" + (prefixPath / "home").string(), "LD_LIBRARY_PATH=" + (runtimePath / "lib").string(), "PATH=" + (runtimePath / "bin").string(), "WINELOADER=" + (runtimePath / "arm64-v8a/bin/wine").string()},
-              serverProcess{runtimePath / "arm64-v8a/bin/wineserver", {"--foreground", "--persistent"}, envVars} {
-        Launch("wineboot.exe", {"--init"}, {}).WaitForExit();
-    }
+WineContext::WineContext(std::filesystem::path pRuntimePath, std::filesystem::path pPrefixPath)
+        : runtimePath{std::move(pRuntimePath)}, prefixPath{std::move(pPrefixPath)},
+          envVars{"WINEPREFIX=" + (prefixPath / "pfx").string(), "HOME=" + (prefixPath / "home").string(), "LD_LIBRARY_PATH=" + (runtimePath / "lib").string(), "PATH=" + (runtimePath / "bin").string(), "WINELOADER=" + (runtimePath / "arm64-v8a/bin/wine").string()},
+          serverProcess{runtimePath / "arm64-v8a/bin/wineserver", {"--foreground", "--persistent"}, envVars} {
+    Launch("wineboot.exe", {"--init"}, {}).WaitForExit();
+}
 
-    Process WineContext::Launch(std::string exe, std::vector<std::string> args, std::vector<std::string> pEnvVars) {
-        pEnvVars.insert(pEnvVars.end(), envVars.begin(), envVars.end());
-        args.insert(args.begin(), exe);
-        return Process{runtimePath / "arm64-v8a/bin/wine", args, pEnvVars};
-    }
+Process WineContext::Launch(std::string exe, std::vector<std::string> args, std::vector<std::string> pEnvVars) {
+    pEnvVars.insert(pEnvVars.end(), envVars.begin(), envVars.end());
+    args.insert(args.begin(), exe);
+    return Process{runtimePath / "arm64-v8a/bin/wine", args, pEnvVars};
+}
 
-    WineContext::~WineContext() {
-        Launch("wineboot.exe", {"--end-session", "--shutdown"}).WaitForExit();
-        Process{runtimePath / "arm64-v8a/bin/wineserver", {"--kill"}, envVars}.WaitForExit();
-    }
+WineContext::~WineContext() {
+    Launch("wineboot.exe", {"--end-session", "--shutdown"}).WaitForExit();
+    Process{runtimePath / "arm64-v8a/bin/wineserver", {"--kill"}, envVars}.WaitForExit();
+}
 }
--- app/src/main/cpp/cassia/wine_ctx.h
+++ app/src/main/cpp/cassia/wine_ctx.h
@@ -4,31 +4,31 @@
 #include "process.h"
 
 namespace cassia {
+/**
+ * @brief A class consolidating all Wine-related processes/state for a specific prefix with convenience wrappers.
+ */
+class WineContext {
+  private:
+    std::filesystem::path runtimePath;
+    std::filesystem::path prefixPath;
+    std::vector<std::string> envVars;
+    Process serverProcess;
+
+  public:
     /**
-     * @brief A class consolidating all Wine-related processes/state for a specific prefix with convenience wrappers.
+     * @details This will start the wineserver process and initialize the Wine prefix with wineboot.
      */
-    class WineContext {
-    private:
-        std::filesystem::path runtimePath;
-        std::filesystem::path prefixPath;
-        std::vector<std::string> envVars;
-        Process serverProcess;
+    WineContext(std::filesystem::path runtimePath, std::filesystem::path prefixPath);
 
-    public:
-        /**
-         * @details This will start the wineserver process and initialize the Wine prefix with wineboot.
-         */
-        WineContext(std::filesystem::path runtimePath, std::filesystem::path prefixPath);
+    /**
+     * @brief Launches a Windows executable in the Wine environment.
+     * @param exe The path to the executable to launch, this doesn't need to be an absolute path for executables in Wine's PATH (eg. cmd.exe, wineboot.exe, etc).
+     */
+    Process Launch(std::string exe, std::vector<std::string> args = {}, std::vector<std::string> envVars = {});
 
-        /**
-         * @brief Launches a Windows executable in the Wine environment.
-         * @param exe The path to the executable to launch, this doesn't need to be an absolute path for executables in Wine's PATH (eg. cmd.exe, wineboot.exe, etc).
-         */
-        Process Launch(std::string exe, std::vector<std::string> args = {}, std::vector<std::string> envVars = {});
-
-        /**
-         * @details This will attempt to shutdown the Wine prefix with wineboot and use wineserver to kill all other wine processes.
-         */
-        ~WineContext();
-    };
+    /**
+     * @details This will attempt to shutdown the Wine prefix with wineboot and use wineserver to kill all other wine processes.
+     */
+    ~WineContext();
+};
 }
