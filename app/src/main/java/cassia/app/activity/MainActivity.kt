cd85584c8bca3184e2716853216abab14272644c
PixelyIon <pixelyion@protonmail.com>
2023-12-21 20:09:33 +0530

    Improve test UI
    The UI for testing has been made more robust for testing in a loop to ensure that all cleanup is being done correctly.

--- app/src/main/java/cassia/app/activity/MainActivity.kt
+++ app/src/main/java/cassia/app/activity/MainActivity.kt
@@ -5,7 +5,9 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Button
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
@@ -18,6 +20,7 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
 import cassia.app.CassiaApplication
 import cassia.app.store.Prefix
 import cassia.app.ui.theme.CassiaTheme
@@ -30,53 +33,89 @@ class MainActivity : ComponentActivity() {
         setContent {
             CassiaTheme {
                 Surface(color = MaterialTheme.colorScheme.background) {
-                    Column(modifier = Modifier.fillMaxSize(), horizontalAlignment = Alignment.CenterHorizontally, verticalArrangement = Arrangement.Center) {
+                    Column(modifier = Modifier.fillMaxSize(), horizontalAlignment = Alignment.Start, verticalArrangement = Arrangement.Center) {
+                        var starting by remember { mutableStateOf(false) }
+                        var stopping by remember { mutableStateOf(false) }
                         var running by remember { mutableStateOf(false) }
                         var reset by remember { mutableStateOf(true) }
+                        var looping by remember { mutableStateOf(false) }
 
                         var prefix by remember { mutableStateOf<Prefix?>(null) }
                         var defaultRuntime by remember { mutableStateOf<String?>(null) }
-                        Text(text = prefix?.uuid ?: "No prefix detected")
-                        Text(text = prefix?.runtimeId ?: "No runtime detected")
+                        Text(text = "Prefix: ${prefix?.uuid ?: "No prefix detected"}")
+                        Text(text = "Runtime: ${prefix?.runtimeId ?: "No runtime detected"}")
 
-                        Text(text = if (running) "Running" else "Stopped")
+                        var counter by remember { mutableStateOf(0) }
+                        Text(text = "Run Counter: $counter")
 
-                        Button(onClick = {
-                            if (running) {
-                                MainScope().launch {
-                                    CassiaApplication.instance.manager.stop()
-                                    running = false
-                                    reset = true
-                                }
-                            } else {
-                                prefix?.let {
+                        Text(text = "Status: " + if (running) "Running" else if (starting) "Started" else if (stopping) "Stopping" else "Stopped")
+
+                        Row {
+                            Button(onClick = {
+                                if (running) {
                                     MainScope().launch {
-                                        CassiaApplication.instance.manager.start(it.uuid)
-                                        running = true
+                                        stopping = true
+                                        CassiaApplication.instance.manager.stop()
+                                        stopping = false
+                                        running = false
+                                        reset = true
+                                    }
+                                } else {
+                                    prefix?.let {
+                                        MainScope().launch {
+                                            starting = true
+                                            CassiaApplication.instance.manager.start(it.uuid)
+                                            running = true
+                                            starting = false
+                                            counter++
+                                        }
                                     }
                                 }
+                            }, enabled = prefix != null && !starting && !stopping && !looping, modifier = Modifier.padding(horizontal = 8.dp)) {
+                                Text(text = if (running) "Stop" else "Start")
                             }
-                        }, enabled = prefix != null) {
-                            Text(text = if (running) "Stop" else "Start")
-                        }
-                        Button(onClick = {
-                            defaultRuntime?.let {
-                                MainScope().launch {
-                                    prefix = CassiaApplication.instance.prefixes.create("Default", it)
+                            Button(onClick = {
+                                prefix?.let {
+                                    looping = !looping
+                                    MainScope().launch {
+                                        while (looping) {
+                                            starting = true
+                                            CassiaApplication.instance.manager.start(it.uuid)
+                                            running = true
+                                            starting = false
+                                            stopping = true
+                                            CassiaApplication.instance.manager.stop()
+                                            stopping = false
+                                            running = false
+                                            reset = true
+                                            counter++
+                                        }
+                                    }
                                 }
+                            }, enabled = prefix != null && (looping || !starting && !running && !stopping)) {
+                                Text(text = if (looping) "Stop Loop" else "Start Loop")
                             }
-                        }, enabled = !running && defaultRuntime != null) {
-                            Text(text = "Create Prefix (Default Runtime: ${defaultRuntime ?: "None"})")
                         }
-                        Button(enabled = !running && reset && prefix != null, onClick = {
-                            prefix?.let {
-                                MainScope().launch {
-                                    prefix = CassiaApplication.instance.prefixes.reset(it.uuid)
-                                    reset = false
+                        Row {
+                            Button(onClick = {
+                                defaultRuntime?.let {
+                                    MainScope().launch {
+                                        prefix = CassiaApplication.instance.prefixes.create("Default", it)
+                                    }
                                 }
+                            }, enabled = !running && defaultRuntime != null, modifier = Modifier.padding(horizontal = 8.dp)) {
+                                Text(text = "Create Prefix (Default)")
                             }
-                        }) {
-                            Text(text = "Reset")
+                            Button(enabled = !running && reset && prefix != null, onClick = {
+                                prefix?.let {
+                                    MainScope().launch {
+                                        prefix = CassiaApplication.instance.prefixes.reset(it.uuid)
+                                        reset = false
+                                    }
+                                }
+                            }) {
+                                Text(text = "Reset Prefix")
+                            }
                         }
 
                         LaunchedEffect(Unit) {
