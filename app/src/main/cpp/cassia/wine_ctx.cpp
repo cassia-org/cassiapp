e02038b28275a7ddb351ac72beb2bde438a35710
PixelyIon <pixelyion@protonmail.com>
2024-02-25 02:13:05 +0530

    Set `WINEDEBUG` based on `cassia.wine.debug` property
    A developer-friendly way to change the debug values without constantly needing to recompile and redeploy the app was required and system properties set via `setprop` fit.

--- app/src/main/cpp/cassia/wine_ctx.cpp
+++ app/src/main/cpp/cassia/wine_ctx.cpp
@@ -1,11 +1,23 @@
 // Copyright © 2023 Cassia Developers, all rights reserved.
 
 #include "wine_ctx.h"
+#include <sys/system_properties.h>
 
 namespace cassia {
+std::string GetWineDebug() {
+    auto property{__system_property_find("cassia.wine.debug")};
+    if (property) {
+        char value[PROP_VALUE_MAX];
+        int len{__system_property_read(property, nullptr, value)};
+        if (len > 0)
+            return "WINEDEBUG=" + std::string{value, static_cast<size_t>(len)};
+    }
+    return "";
+}
+
 WineContext::WineContext(std::filesystem::path pRuntimePath, std::filesystem::path pPrefixPath)
         : runtimePath{std::move(pRuntimePath)}, prefixPath{std::move(pPrefixPath)},
-          envVars{"WINEPREFIX=" + (prefixPath / "pfx").string(), "HOME=" + (prefixPath / "home").string(), "LD_LIBRARY_PATH=" + (runtimePath / "lib").string(), "PATH=" + (runtimePath / "bin").string(), "WINELOADER=" + (runtimePath / "bin/wine").string()},
+          envVars{"WINEPREFIX=" + (prefixPath / "pfx").string(), "HOME=" + (prefixPath / "home").string(), "LD_LIBRARY_PATH=" + (runtimePath / "lib").string(), "PATH=" + (runtimePath / "bin").string(), "WINELOADER=" + (runtimePath / "bin/wine").string(), GetWineDebug()},
           serverProcess{runtimePath / "bin/wineserver", {"--foreground", "--persistent"}, envVars, Logger::GetPipe("wineserver")} {
     Launch("wineboot.exe", {"--init"}, {}, Logger::GetPipe("wineboot")).WaitForExit();
 }
